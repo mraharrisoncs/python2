a66eae732687724cdda01693ee7b36b6d2a29736
Alan Harrison <alan@httcs.online>
2025-05-04 23:18:31 +0100

    tabbed testing

--- boxers1.py
+++ boxers1.py
@@ -1,16 +1,17 @@
+
+reds =  [10,8,7,9,10,10,9,8,7,9]
+blues = [17,9,6,8,7,4,10,8,7,9]
+
 redtotal=0
-red=[10,8,7,9,10,10,9,8,7,9]
 for i in range(10):
-  redtotal = redtotal + red[i]
-# subtract max and min
+  redtotal = redtotal + reds[i]
 print("red scores",redtotal)
 
 bluetotal=0
-blue=[17,9,6,8,7,4,10,8,7,9]
 for i in range(10):
-  bluetotal = bluetotal + blue[i]
-bluetotal = bluetotal - max(blue) - min(blue) 
+  bluetotal = bluetotal + blues[i]
 print("blue scores",bluetotal)
+
 if bluetotal > redtotal:
     print("blue wins!")
 elif bluetotal == redtotal:
--- draw.py
+++ draw.py
@@ -5,4 +5,5 @@ print("drawing!")
 shapes.square(0,0,200,'magenta',True)
 shapes.triangle(0,200,200,'red',True)
 shapes.mycircle(-100,200,50,'yellow',True,360)
-print("finished!")
+x = input("finished!")
+
